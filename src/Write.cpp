c31984e4c1cce46a7ecbaf6d217143c4787ffc3d
PBeGood4 <pedrocbeirao@gmail.com>
2022-10-29 12:01:34 +0100

    Very epic shit part 2

--- src/Write.cpp
+++ src/Write.cpp
@@ -27,48 +27,13 @@ bool Write::remove_class(string studentcode, string classcode)
 
 bool Write::remove_uc(string studentcode, string uccode)
 {
-    ifstream fi;
-    ofstream fo;
-    fi.open("../students_classes.csv");
-    fo.open("../students_classeso.csv");
-    if (!fi.is_open() || !fo.is_open())
+    auto it = remove_if(s.students_classes_v.begin(), s.students_classes_v.end(), [uccode, studentcode](students_classes r) { return (r.UcCode == uccode && (r.StudentCode == studentcode || r.StudentName == studentcode)); });
+    if (it != s.students_classes_v.end())
     {
-        cout << "\nCould not open file" << endl;
-        return false;
+        s.students_classes_v.erase(it, s.students_classes_v.end());
+        return true;
     }
-
-    bool rt = false;
-
-    string buffer;
-    getline(fi, buffer, '\n');
-    fo << buffer << "\n";
-    while (getline(fi, buffer, '\n'))
-    {
-        stringstream line(buffer);
-        string buf;
-        string stu1;
-        string stu2;
-        getline(line, stu1, ',');
-        getline(line, stu2, ',');
-        getline(line, buf, ',');
-        if ((stu1 != studentcode && stu2 != studentcode) || buf != uccode)
-        {
-            fo << buffer << "\n";
-        }
-        else
-        {
-            rt = true;
-        }
-        getline(line, buf, '\r');
-    }
-
-    fi.close();
-    fo.close();
-
-    remove("../students_classes.csv");
-    rename("../students_classeso.csv", "../students_classes.csv");
-
-    return rt;
+    return false;
 }
 
 bool Write::remove_uc_class(string studentcode, string uccode, string classcode)
