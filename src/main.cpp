0dc1e4c3e6280808400c7128e8b56260405274b6
Lauranea <93668178+Lauranea@users.noreply.github.com>
2022-11-04 15:41:46 +0000

    -cenas

--- src/main.cpp
+++ src/main.cpp
@@ -162,19 +162,7 @@ void test(Scheduler &s, bool keep)
     }
 }
 
-/**
- * @brief 
- * Imprime as opções para o utilizador escolher( view, request, process request);
- * Utiliza comandos vim para guardar e sair;
- * Lê o input colocado do utilizador, e dependendo dele, imprime mais opções para o utilizador escolher o que pretender;
- * No view, possibilita a visualização de forma ordenada de turmas, de alunos, da ocupação em UCs e dos horários quer do aluno, quer da Turma;
- * Em Requests, adiciona os pedidos de remoção, adição ou troca de turma de um aluno a uma queue;
- * Em Process requests, da para ou ver os pedidos, ou processar(aonde processa os requests possiveis, e mantem numa queue os que nao foram possiveis);
- * Complexidade: m(entre 1 (se o utilizador der logo quit) e infinito (se o utilizador continuar a usar o programa))* n
- * @param argc 
- * @param argv 
- * @return int 
- */
+
 int main(int argc, char **argv)
 {
     Scheduler s;
